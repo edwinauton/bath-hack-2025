613b066a8ad6069e6cd82f35dae5f4e64d36d8e0
Laura <laura@oddy.net>
2025-04-05 16:36:16 +0100

    Profile page updates

---
+++ src/components/Badges.vue
@@ -0,0 +1,33 @@
+<script setup>
+    import { reactive } from 'vue';
+
+    const badges = reactive([
+        {
+            name: "First Badge",
+            description: "This is the first badge",
+        },
+        {
+            name: "Second Badge",
+            description: "This is the second badge",
+        },
+        {
+            name: "Third Badge",
+            description: "This is the third badge",
+        }
+    ]);
+</script>
+
+<template>
+    <div>
+        <ul>
+            <li v-for="badge in badges" :key="badge.name">
+                <h3>{{ badge.name }}</h3>
+                <p>{{ badge.description }}</p>
+            </li>
+        </ul>
+    </div>
+    
+
+</template>
+
+<style scoped></style>
