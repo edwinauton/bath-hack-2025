5b9dabeaafdb7ff11fd8099ea399410abd936268
edwinauton <ea994@bath.ac.uk>
2025-04-05 13:15:06 +0100

    style: Run Prettier

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import "./assets/styles/main.css";
+import router from "./utils/router";
+import "./assets/styles/styles.css";
 
 const app = createApp(App);
+
+app.provide("router", router);
+app.use(router);
+
 app.mount("#app");
